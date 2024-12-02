11cebd3f4afa12c851dbe1ae1ef100939be2440c
cvijayap <cvijayap@LIN-5CG204593H.corp.capgemini.com>
2024-12-02 11:01:15 +0530

    fourth commit

--- Application1/src/main/java/com/Application1/in/Exceptions/MethodArgumentTypeMismatchException.java
+++
@@ -1,8 +0,0 @@
-package com.Application1.in.Exceptions;
-
-public class MethodArgumentTypeMismatchException extends RuntimeException {
-    
-    public MethodArgumentTypeMismatchException(String message) {
-        super(message);
-    }
-}
